461afc0d59f9cf479d3d43141a50744c2c110eea
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-13 17:40:36 +0900

    Todoコンポーネントの要素削除

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,7 +5,6 @@ const Todo = () => {
     <div className="card">
       <h2>TITLE</h2>
       <div className="actions">
-        <span>A Span</span>
         <button className="btn">Delete</button>
       </div>
     </div>
