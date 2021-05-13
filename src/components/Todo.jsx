ca825cbaf172e238d269ec609d3b2e85be26fea2
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-13 17:40:04 +0900

    Todoコンポーネントに要素追加

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,6 +5,7 @@ const Todo = () => {
     <div className="card">
       <h2>TITLE</h2>
       <div className="actions">
+        <span>A Span</span>
         <button className="btn">Delete</button>
       </div>
     </div>
