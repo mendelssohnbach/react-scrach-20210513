ff68a0e3091b7bc05ee3bda84d1d015787cc7081
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-13 19:00:09 +0900

    イベントハンドリング
    親コンポーネントの属性を受け取り、その値をコンソール出力

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,17 @@
 import '../../src/index.css';
 
 const Todo = (props) => {
+  const deleteHandler = () => {
+    console.log('Clicked');
+    console.log(props.text);
+  };
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
